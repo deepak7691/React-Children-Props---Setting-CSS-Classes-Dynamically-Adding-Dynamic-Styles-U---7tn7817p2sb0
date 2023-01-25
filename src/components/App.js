e5a5d0343aac752ed757c349ec4b3d3b3bad78b2
deepak7691 <kawliya11@gmail.com>
2023-01-25 15:46:17 +0530

    Kawliya11

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,21 @@
-import React , {useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import '../styles/App.css';
 import ChildComponent from './ChildComponent';
 const App = () => {
- //code here
+  const [input1, SetInput1] = useState(0)
 
   return (
     <div id="main">
-      <ChildComponent /> 
-       <input id='input' onChange={} />
-    
-      <p id='output'>{}</p>
+      <ChildComponent input={
+        <input id='input' onChange={(e) => {
+          let res = e.target.value;
+          res = Math.pow(res, 2);
+          SetInput1(res)
+
+        }} />}
+      />
+
+      <p id='output'>{input1}</p>
     </div>
   )
 }
